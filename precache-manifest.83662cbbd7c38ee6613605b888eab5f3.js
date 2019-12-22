1edc111c97c6614c07a5f4c2bad769f40708954c
eunicLab <s1787807@FVFTXNHCJ1WKs-MacBook-Air.local>
2019-12-22 05:03:47 +0100

    Updates

---
+++ precache-manifest.83662cbbd7c38ee6613605b888eab5f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2566652d6c8f60d01d604f01d47f331d",
+    "url": "/my_to_do_app/index.html"
+  },
+  {
+    "revision": "c5ef8f37c47d710f5e08",
+    "url": "/my_to_do_app/static/css/main.204c99e0.chunk.css"
+  },
+  {
+    "revision": "4daadc0588b11b84dd19",
+    "url": "/my_to_do_app/static/js/2.78793fa5.chunk.js"
+  },
+  {
+    "revision": "c5ef8f37c47d710f5e08",
+    "url": "/my_to_do_app/static/js/main.8ffb4461.chunk.js"
+  },
+  {
+    "revision": "559f8024a571df173ab1",
+    "url": "/my_to_do_app/static/js/runtime-main.7a8fb670.js"
+  }
+]);
