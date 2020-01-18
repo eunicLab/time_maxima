e234e58d1b75a0bf818c1a2f278c302fe9b0ba74
eunicLab <s1787807@FVFTXNHCJ1WKs-MacBook-Air.local>
2020-01-18 12:45:45 +0100

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# my_to_do_app
-this app helps users manage to do items
---
+++ precache-manifest.aeb4606320ddf487aa267b472af84ae1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ae6b70e10f239036175a3ade0fd6ef8",
+    "url": "/my_to_do_app/index.html"
+  },
+  {
+    "revision": "c7d712cb1e4f46758096",
+    "url": "/my_to_do_app/static/css/main.f05691ad.chunk.css"
+  },
+  {
+    "revision": "28e5ed6082f9fb2dcc37",
+    "url": "/my_to_do_app/static/js/2.65bf8179.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my_to_do_app/static/js/2.65bf8179.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c7d712cb1e4f46758096",
+    "url": "/my_to_do_app/static/js/main.316e51b2.chunk.js"
+  },
+  {
+    "revision": "e34ca0a10cb953bcfcbd",
+    "url": "/my_to_do_app/static/js/runtime-main.a2b2472a.js"
+  },
+  {
+    "revision": "1cd05d5304fc871e995e663987584f51",
+    "url": "/my_to_do_app/static/media/delete_icon.1cd05d53.png"
+  },
+  {
+    "revision": "e764b0709e1a1fe6032ca3ee6b6f6729",
+    "url": "/my_to_do_app/static/media/edit_icon.e764b070.png"
+  }
+]);
