66a459f3d481e7ef5eaa78a721923aaa99fde992
eunicLab <eunicennaji@hotmail.co.uk>
2020-03-15 05:05:25 +0100

    make better

--- src/App.js
+++ src/App.js
@@ -1,208 +1,46 @@
-import React from "react"
-import TodoItem from "./TodoItem"
-import './App.css';
-import axios from 'axios';
+import React from 'react';
+import Login from './Login.js';
+import ToDoApp from './ToDoApp.js';
+import './ToDoApp.css';
 
-
-
-class App extends React.Component
-{
-  constructor(props)
-  {
+class App extends React.Component {
+  constructor(props) {
     super(props);
-  
-    global.clickCount = 0
-    global.inputArray = []
-    global.backendData=""
-  
- 
-  
-
-    this.state ={
-                  textboxStatus: "noTextBox",
-                  userInput: "",
-                  todos: global.inputArray,
-                  dateInput:"",
-                  timeInput:"",
-                  alert:"",
-                }
-
-
-   
-    this.handleAdd = this.handleAdd.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.handleChange=this.handleChange.bind(this)
-    this.handleCancel = this.handleCancel.bind(this)
-    this.handleDate = this.handleDate.bind(this)
-    this.handleTime = this.handleTime.bind(this)
-    
-
-   
-  
-  }
-
-
-
-
-
-
-
-
-
-
-
-  handleAdd(event)
-    {
-      event.preventDefault();
-      this.setState({textboxStatus: "formStyle", userInput: "", dateInput:"", timeInput:"", alert:""})
-    }
-
-
-
-    
-
-
-componentDidMount(){
-  axios.get('https://still-everglades-82859.herokuapp.com/api/stuff',)
-  .then(response=>{ 
-
-
-
-
-
-    if(response.data.length>0) {
-
-    response.data.sort(function(a, b){
-    var dateA=new Date(a.date), dateB=new Date(b.date)
-    return dateA-dateB //sort by date ascending
-})
-
-
-
-}
-
-
-
-this.setState({todos:response.data})
-
- })
-  
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-  handleSubmit(event) {
-
-  if (this.state.userInput!=="")
-      {
-
-
-        global.inputArray[global.clickCount]={id:"", text:this.state.userInput, date:this.state.dateInput, time:this.state.timeInput} 
-         
-         axios.post('https://still-everglades-82859.herokuapp.com/api/stuff', global.inputArray[global.clickCount])
-         .then(res=> {window.location = '/';}
-)
-
-     
-
-        global.inputArray.sort(function(a, b){
-    var dateA=new Date(a.date), dateB=new Date(b.date)
-    return dateA-dateB //sort by date ascending
-})
-        event.preventDefault();
-        this.setState({textboxStatus: "noTextBox"})
-        global.clickCount++
-
-       
-      } 
-    else {
-            event.preventDefault();
-            this.setState({textboxStatus: "formStyle", userInput: "", dateInput:""})
-          }
+    this.state = {
+      data: '',
+      email: '',
+      firstName: ''
     };
-
-
-  handleCancel(event){
-    event.preventDefault();
-    this.setState({textboxStatus: "noTextBox", userInput: "", dateInput:"", timeInput: ""})
   }
 
+  callbackFunction = childData => {
+    this.setState({ data: childData });
+  };
 
-  handleDate(event)
-    {
-      this.setState({ dateInput: event.target.value})
+  callbackFunction2 = childEmail => {
+    this.setState({ email: childEmail });
+  };
+  callbackFunction3 = childFirstName => {
+    this.setState({ firstName: childFirstName });
+  };
 
-
-      
-    }
-
-
-
-handleTime(event)
-    {
-      this.setState({ timeInput: event.target.value})
-    }
-
-
-
-
-  handleChange(event)
-    {
-      this.setState({ userInput: event.target.value})
-    }
-
-
-
-
-
-
-
-
-
-render(){
-              
-                const todoItems = this.state.todos.map(item =><TodoItem item={item} handleEdit={this.handleEditParent}/>)
-
-        return(
-
-          <div className="App">
-           <header className="App-header">
-          <div className="todo-list">
-           <h6 className="motivation">Tick says the Clock...Do what you have to do</h6>
-
-               <form className={this.state.textboxStatus}>
-                    <input type ="text" name= "userInput" placeholder ="To do Item"value={this.state.userInput} onChange={this.handleChange}/><br/>
-                    <input type="date"  name="date" placeholder="Due Date" value={this.state.dateInput} onChange={this.handleDate}/><br/>
-                    <input type="time"  name="time" placeholder="Time" value={this.state.timeInput} onChange={this.handleTime}/><br/>
-                    <button className="formButton" onClick={this.handleSubmit}> OK </button>
-                    <button className="formButton" onClick={this.handleCancel}>CANCEL</button>
-                    
-                  </form>
-                 
-                <button className="btn" onClick={this.handleAdd}>
-                  Add a New Item
-                </button>
-               
-    
-            
-            {todoItems}
-
-          </div>
-           </header>
-          </div>
-              )   
-
-        }
+  render() {
+    return (
+      <div>
+        <Login
+          parentCallback={this.callbackFunction}
+          parentCallback2={this.callbackFunction2}
+          parentCallback3={this.callbackFunction3}
+          parentCallback4={this.callbackFunction4}
+        />
+        <ToDoApp
+          dataFromParent={this.state.data}
+          emailFromParent={this.state.email}
+          firstNameFromParent={this.state.firstName}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,240 +1,285 @@
-import React from "react"
-import './App.css';
+import React from 'react';
+import './ToDoApp.css';
 import edit from './edit_icon.png';
-import delete_icon from './delete_icon.png'
+import delete_icon from './delete_icon.png';
 import axios from 'axios';
 
-
-
-
 class TodoItem extends React.Component {
-	constructor(){
-		super()
-		this.state = {checkBoxStatus:false, styleText:"unchecked", reminder: null, alert:false, editBoxStatus:"noTextBox",
-						userInput: "", dateInput:"",
-                  		timeInput:"", 
-	}
+  constructor(props) {
+    super(props);
+    global.updateCount = 0;
+    this.state = {
+      checkBoxStatus: false,
+      styleText: 'unchecked',
+      reminder: null,
+      alert: false,
+      editBoxStatus: 'noTextBox',
+      userInput: '',
+      dateInput: '',
+      timeInput: '',
+      updateCount: global.updateCount
+    };
 
-		
-	this.handleBoxChange=this.handleBoxChange.bind(this)
-	this.reminderLogic= this.reminderLogic.bind(this)
-	this.handleDelete= this.handleDelete.bind(this)
-	this.handleChange=this.handleChange.bind(this)
-    this.handleCancel = this.handleCancel.bind(this)
-    this.handleDate = this.handleDate.bind(this)
-    this.handleTime = this.handleTime.bind(this)
-    this.handleEdit=this.handleEdit.bind(this)
-  	this.handleUpdate = this.handleUpdate.bind(this)
-  	this.reminder= this.reminder.bind(this)
-
-	}
-
-
-
-
-
-
-  reminderLogic (){
-
-	var curTime = new Date()	
-	var year= curTime.getFullYear()
-	var month= curTime.getMonth()
-	var day = curTime.getDate()
-	
-	var end = new Date(this.props.item.date);
-	var year2= end.getFullYear()
-	var month2= end.getMonth()
-	var day2 = end.getDate()
-	
-	
-	var day3;
-	var future =day2-day
-	var past = day-day2
-
-	var futureMonth = month2-month;
-	var pastMonth = month-month2;
-	var futureYear = year2-year;
-	var pastYear = year-year2;
-	if (futureYear===1){day3=false};
-	if (futureYear ===-1){day3=true};
-	if (futureYear >1){day3=false}
-	if (futureYear<1){day3=true}
-	if (futureMonth >0 && futureYear===0){day3=false}
-	if (futureMonth<0 && futureYear===0){day3=true}	
-	if (future>0 && futureMonth===0 && futureYear===0){day3=false} 
-	if (future<0 && futureMonth===0 && futureYear===0){day3=true}
-	if (future===0 && futureMonth===0 && futureYear===0){day3=true}
-
-		return day3;
-	
+    this.handleBoxChange = this.handleBoxChange.bind(this);
+    this.reminderLogic = this.reminderLogic.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
+    this.handleDate = this.handleDate.bind(this);
+    this.handleTime = this.handleTime.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
+    this.reminder = this.reminder.bind(this);
   }
 
+  reminderLogic() {
+    var curTime = new Date();
+    var year = curTime.getFullYear();
+    var month = curTime.getMonth();
+    var day = curTime.getDate();
 
+    var end = new Date(this.props.item.date);
+    var year2 = end.getFullYear();
+    var month2 = end.getMonth();
+    var day2 = end.getDate();
 
+    var day3;
+    var future = day2 - day;
+    var past = day - day2;
 
-
-reminder(){
-      var curTime = new Date()  
-  var year= curTime.getFullYear()
-  var month= curTime.getMonth()
-  var day = curTime.getDate()
-  
-  var end = new Date(this.props.item.date);
-  var year2= end.getFullYear()
-  var month2= end.getMonth()
-  var day2 = end.getDate()
-
-
-  var day3
-  var future =day2-day
-  var past = day-day2
-
-  var futureMonth = month2-month;
-  var pastMonth = month-month2;
-  var futureYear = year2-year;
-  var pastYear = year-year2;
-  if (futureYear===1){day3="Due next year"};
-  if (futureYear ===-1){day3="Due last year"}
-  if (futureYear >1){day3="due in "+futureYear+" year(s) time"};
-  if (futureYear<1){day3="due " +pastYear+" year(s) ago"};
-  if (futureMonth >0 && futureYear===0){day3="Due in "+futureMonth+" month(s) time"};
-  if (futureMonth<0 && futureYear===0){day3="Due "+pastMonth+" month(s) ago"};
-  if (future>0 && futureMonth===0 && futureYear===0){day3 ="Due in "+future+" day(s)"};
-  if (future<0 && futureMonth===0 && futureYear===0){day3="Due "+past+ " day(s) ago"};
-  if (future===0 && futureMonth===0 && futureYear===0){day3="Due today"};
-
-
-
-  return day3
-
+    var futureMonth = month2 - month;
+    var pastMonth = month - month2;
+    var futureYear = year2 - year;
+    var pastYear = year - year2;
+    if (futureYear === 1) {
+      day3 = false;
+    }
+    if (futureYear === -1) {
+      day3 = true;
+    }
+    if (futureYear > 1) {
+      day3 = false;
+    }
+    if (futureYear < 1) {
+      day3 = true;
+    }
+    if (futureMonth > 0 && futureYear === 0) {
+      day3 = false;
+    }
+    if (futureMonth < 0 && futureYear === 0) {
+      day3 = true;
+    }
+    if (future > 0 && futureMonth === 0 && futureYear === 0) {
+      day3 = false;
+    }
+    if (future < 0 && futureMonth === 0 && futureYear === 0) {
+      day3 = true;
+    }
+    if (future === 0 && futureMonth === 0 && futureYear === 0) {
+      day3 = true;
     }
 
+    return day3;
+  }
 
+  reminder() {
+    var curTime = new Date();
+    var year = curTime.getFullYear();
+    var month = curTime.getMonth();
+    var day = curTime.getDate();
 
+    var end = new Date(this.props.item.date);
+    var year2 = end.getFullYear();
+    var month2 = end.getMonth();
+    var day2 = end.getDate();
 
- handleDate(event)
-    {
-      this.setState({ dateInput: event.target.value})
+    var day3;
+    var future = day2 - day;
+    var past = day - day2;
 
-
-      
+    var futureMonth = month2 - month;
+    var pastMonth = month - month2;
+    var futureYear = year2 - year;
+    var pastYear = year - year2;
+    if (futureYear === 1) {
+      day3 = 'Due next year';
+    }
+    if (futureYear === -1) {
+      day3 = 'Due last year';
+    }
+    if (futureYear > 1) {
+      day3 = 'due in ' + futureYear + ' year(s) time';
+    }
+    if (futureYear < 1) {
+      day3 = 'due ' + pastYear + ' year(s) ago';
+    }
+    if (futureMonth > 0 && futureYear === 0) {
+      day3 = 'Due in ' + futureMonth + ' month(s) time';
+    }
+    if (futureMonth < 0 && futureYear === 0) {
+      day3 = 'Due ' + pastMonth + ' month(s) ago';
+    }
+    if (future > 0 && futureMonth === 0 && futureYear === 0) {
+      day3 = 'Due in ' + future + ' day(s)';
+    }
+    if (future < 0 && futureMonth === 0 && futureYear === 0) {
+      day3 = 'Due ' + past + ' day(s) ago';
+    }
+    if (future === 0 && futureMonth === 0 && futureYear === 0) {
+      day3 = 'Due today';
     }
 
+    return day3;
+  }
 
+  handleDate(event) {
+    this.setState({ dateInput: event.target.value });
+  }
 
-handleTime(event)
-    {
-      this.setState({ timeInput: event.target.value})
-    }
+  handleTime(event) {
+    this.setState({ timeInput: event.target.value });
+  }
 
+  handleChange(event) {
+    this.setState({ userInput: event.target.value });
+  }
 
+  handleBoxChange(event) {
+    this.setState({
+      checkBoxStatus: event.target.checked
+    });
+  }
 
-
-  handleChange(event)
-    {
-      this.setState({ userInput: event.target.value})
-    }
-
-
-
-
-
-
-	handleBoxChange(event){
-		this.setState({ 
-			checkBoxStatus: event.target.checked
-
-		})
-
-	}
-
-
-
-handleEdit(event){
-event.preventDefault();
-
-this.setState({editBoxStatus:"formStyle", userInput:this.props.item.text, dateInput:this.props.item.date, timeInput:this.props.item.time})
-     
-}
-
-
-
-
-
-handleUpdate(event){
-	event.preventDefault();
-
-	const upDatedList={
-		text:this.state.userInput,
-		date:this.state.dateInput,
-		time:this.state.timeInput,
-
-	}
-
-
-
-  axios.put('https://still-everglades-82859.herokuapp.com/api/stuff/'+this.props.item._id, upDatedList)
-	.then(res=>{window.location = '/';
-});
-
-	this.setState({editBoxStatus:"noTextBox",})
-
-
-
-	
-}
-
-
-handleCancel(event){
+  handleEdit(event) {
     event.preventDefault();
-    this.setState({editBoxStatus:"noTextBox"})
+
+    this.setState({
+      editBoxStatus: 'formStyle',
+      userInput: this.props.item.text,
+      dateInput: this.props.item.date,
+      timeInput: this.props.item.time
+    });
   }
 
+  handleUpdate(event) {
+    event.preventDefault();
 
+    const upDatedList = {
+      text: this.state.userInput,
+      date: this.state.dateInput,
+      time: this.state.timeInput
+    };
 
-	
-	handleDelete(event){
-		console.log(this.props.item._id)
-   
-    axios.delete('https://still-everglades-82859.herokuapp.com/api/stuff/'+this.props.item._id)
-    .then(res=> {window.location = '/';
-});
-}
+    axios
+      .put(
+        'https://still-everglades-82859.herokuapp.com/api/stuff/' +
+          this.props.item._id,
+        upDatedList
+      )
+      .then(res => {
+        global.updateCount++;
+        this.setState({ updateCount: global.updateCount });
+        this.sendData();
+      });
 
+    this.setState({
+      editBoxStatus: 'noTextBox'
+    });
+  }
 
+  handleCancel(event) {
+    event.preventDefault();
+    this.setState({ editBoxStatus: 'noTextBox' });
+  }
 
+  sendData = () => {
+    this.props.parentCallback(this.state.updateCount);
+  };
 
-	render(){
+  handleDelete(event) {
+    event.preventDefault();
+    axios
+      .delete(
+        'https://still-everglades-82859.herokuapp.com/api/stuff/' +
+          this.props.item._id
+      )
+      .then(res => {
+        global.updateCount++;
+        this.setState({ updateCount: global.updateCount });
+        this.sendData();
+      });
+  }
 
-
-
+  render() {
     return (
+      <div className='container'>
+        <div className='todo-item'>
+          <input
+            type='checkbox'
+            name='checkbox'
+            checked={this.state.checkBoxStatus}
+            onChange={this.handleBoxChange}
+            className='myCheckBox'
+          />
+          <p className={this.state.checkBoxStatus ? 'checked' : 'unchecked'}>
+            {this.props.item.text}
+          </p>
+          <p className='date'>{this.props.item.date}</p>
 
-    	<div className="container">
-
-        <div className="todo-item">
-            <input type="checkbox" name ="checkbox" checked = {this.state.checkBoxStatus} onChange={this.handleBoxChange}/>
-            <p className={this.state.checkBoxStatus ? "checked": "unchecked"}>{this.props.item.text}</p> 
-            <p className="date" >{this.props.item.date}</p><br/>
-            <img src={edit} className="icon1" onClick = {this.handleEdit}/>
-            <img src={delete_icon} className="icon2" onClick={this.handleDelete}/>
+          <img
+            src={edit}
+            className='icon1'
+            alt='An edit button'
+            onClick={this.handleEdit}
+          />
+          <img
+            src={delete_icon}
+            className='icon2'
+            alt='A delete button'
+            onClick={this.handleDelete}
+          />
         </div>
-         <p className={this.reminderLogic() === true && this.state.checkBoxStatus ==false ? "reminderAlert":"reminder"}>{this.reminder()}</p>
-         
+        <p
+          className={
+            this.reminderLogic() === true && this.state.checkBoxStatus == false
+              ? 'reminderAlert'
+              : 'reminder'
+          }>
+          {this.reminder()}
+        </p>
 
-         <form className={this.state.editBoxStatus}>
-                    <input type ="text" name= "userInput" value={this.state.userInput} onChange={this.handleChange}/><br/>
-                    <input type="date"  name="date"  value={this.state.dateInput} onChange={this.handleDate}/><br/>
-                    <input type="time"  name="time"  value={this.state.timeInput} onChange={this.handleTime}/><br/>
-                    <button className="formButton" onClick={this.handleUpdate}>Update</button>
-                    <button className ="formButton" onClick={this.handleCancel}>CANCEL</button>
-                    
-                  </form>
-
-         </div>
-       
-    )
+        <form className={this.state.editBoxStatus}>
+          <input
+            type='text'
+            name='userInput'
+            value={this.state.userInput}
+            onChange={this.handleChange}
+          />
+          <br />
+          <input
+            type='date'
+            name='date'
+            value={this.state.dateInput}
+            onChange={this.handleDate}
+          />
+          <br />
+          <input
+            type='time'
+            name='time'
+            value={this.state.timeInput}
+            onChange={this.handleTime}
+          />
+          <br />
+          <button className='formButton' onClick={this.handleUpdate}>
+            Update
+          </button>
+          <button className='formButton' onClick={this.handleCancel}>
+            CANCEL
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
-}
 
-export default TodoItem
+export default TodoItem;
